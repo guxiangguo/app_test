220bbd184ca573203535ed7c39a2f2b6a023606f
gxg <253447714@qq.com>
2025-03-25 11:17:43 +0800

    ver 1.0

--- app.c
+++ app.c
@@ -3,6 +3,7 @@
 int main (int argc, char **argv)
 {
   	printf ("Hello test_app (Yocto)!\n");
-	printf ("I am test_app_hello project\n");
+	printf ("I am test_app_hello project\n");	
+	printf ("Create new version tag v1.0\n");
 	return 0;
 }
