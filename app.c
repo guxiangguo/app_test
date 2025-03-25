0f4dae1505bd871e98ad64a5881bb94d66bd1bc7
gxg <253447714@qq.com>
2025-03-25 14:20:18 +0800

    ver 1.2

--- app.c
+++ app.c
@@ -6,5 +6,6 @@ int main (int argc, char **argv)
 	printf ("I am test_app_hello project\n");	
 	printf ("Create new version tag v1.0\n");	
 	printf ("Create new version tag v1.1\n");
+	printf ("Create new version tag v1.2\n");
 	return 0;
 }
